387474ce2f949ebd3c7b20a88a44cd4d3580b610
masterUNG <phrombutr@gmail.com>
2026-06-13 08:45:42 +0700

    update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
     }
 
         signingConfigs {
