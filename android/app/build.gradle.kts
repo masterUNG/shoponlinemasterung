42d58cd148a8cacdba531f335a861a4e9fd78d9a
masterUNG <phrombutr@gmail.com>
2026-06-26 21:22:04 +0700

    chore: update version codes and names in build.gradle.kts and Info.plist

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
     }
 
         signingConfigs {
